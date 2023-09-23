cdb6be06823101aa9d8b4ab7da43f2e9c3428737
DavidLauren <leroydavid@hotmail.fr>
2023-09-23 15:02:04 +0200

    ajout template

---
+++ template_part/modal.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Trigger/Open The Modal -->
+<button id="myBtn">Contact</button>
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">x</span>
+    <p>Some text in the Modal..</p>
+  </div>
+
+</div>
+    
+</body>
+</html>
